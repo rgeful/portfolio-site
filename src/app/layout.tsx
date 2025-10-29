1e116f9fa20878264283d49c15a2e52ef6effb8b
Saul Plascencia <sxul@Sauls-MacBook-Air.local>
2025-10-29 15:11:57 -0700

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import { Geist } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/letter.svg",
+  },
+};
 
 export default function RootLayout({
   children,
